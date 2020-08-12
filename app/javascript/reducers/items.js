c0bbf9a1219add0b1209ef46cbabfc5372c0d268
Ginny Mangat <mangatgrinpal@gmail.com>
2020-08-12 14:14:31 -0700

    added menu item reducer

--- app/javascript/reducers/items.js
+++ app/javascript/reducers/items.js
@@ -2,6 +2,48 @@ import {
 	FETCH_ITEMS_REQUEST,
 	FETCH_ITEMS_SUCCESS,
 	FETCH_ITEMS_FAILURE,
-	ADD_ITEM,
-	DELETE_ITEM
+	ADD_MENU_ITEM,
+	DELETE_MENU_ITEM,
+	SET_ITEM_FORM_VISIBILITY
 } from './types';
+
+const initialState = {
+	menu: [],
+	isFetching: true,
+	itemFormVisible: false
+}
+
+export default function(state = initialState, action) {
+	const { type, payload } = action;
+	switch(type) {
+		case FETCH_ITEMS_REQUEST:
+			return {
+				...state,
+				isFetching: true
+			};
+		case FETCH_ITEMS_SUCCESS:
+			return {
+				...state,
+				itemList: payload,
+				isFetching: false
+			};
+		case FETCH_ITEMS_ERROR:
+			return {
+				...state,
+				isFetching: false
+			};
+		case ADD_MENU_ITEM:
+		case DELETE_MENU_ITEM:
+			return {
+				...state,
+				itemList: payload
+			};
+		case SET_ITEM_FORM_VISIBILITY:
+			return {
+				...state,
+				itemFormVisible: payload
+			};
+		default:
+			return state
+	}
+}
